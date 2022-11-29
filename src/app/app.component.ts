316715982a4af46d18c0126724da08b3842fa3a0
Kseniya <nuncivolt@gmail.com>
2022-11-29 17:21:46 +0300

    Fixed bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   public employees: Employee[] | any;
@@ -24,27 +24,27 @@ export class AppComponent implements OnInit {
 
   public getUdemyCourse(): void {
     console.log('Fetching all courses...');
-      this.employeeService.getUdemyCourse().subscribe(
-        (response: any) => {
-          console.log(response);
-          console.log(response.results);
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-        }
+    this.employeeService.getUdemyCourse().subscribe(
+      (response: any) => {
+        console.log(response);
+        console.log(response.results);
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      }
     );
   }
 
   public getEmployees(): void {
     console.log('Fetching all employees...');
-      this.employeeService.getEmployees().subscribe(
-        (response: Employee[]) => {
-          console.log(response);
-          this.employees = response;
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-        }
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        console.log(response);
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      }
     );
   }
 
@@ -52,11 +52,13 @@ export class AppComponent implements OnInit {
     console.log('Searching employees...');
     const results: Employee[] = [];
     for (const employee of this.employees) {
-      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
-          || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
-          employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
-          employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
-          results.push(employee);
+      if (
+        employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ) {
+        results.push(employee);
       }
     }
     this.employees = results;
@@ -65,38 +67,12 @@ export class AppComponent implements OnInit {
     }
   }
 
-
-  
-
-  /* public addEmployee(employeeForm: NgForm): void {
-    const addFormId = document.getElementById('addFormId');
-    if (addFormId) {
-      addFormId.click();
-    }
-    console.log('Adding employee...', employeeForm.value);
-      this.employeeService.addEmployee(employeeForm.value).subscribe(
-        (response: Employee) => {
-          console.log(response);
-          this.employee = response;
-          this.getEmployees();
-          employeeForm.reset();
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-          employeeForm.reset();
-        }
-    );
-  } */
-
-
-
-
   public addEmployee(addForm: NgForm): void {
     const addFormId = document.getElementById('addFormId');
     if (addFormId) {
       addFormId.click();
     }
- 
+
     this.employeeService.addEmployee(addForm.value).subscribe(
       (response: Employee) => {
         console.log(response);
@@ -108,38 +84,36 @@ export class AppComponent implements OnInit {
         addForm.reset();
       }
     );
-
-
   }
 
   public updateEmployee(employee: Employee): void {
     console.log(`Editing employee...${employee}`);
-      this.employeeService.updateEmployee(employee).subscribe(
-        (response: Employee) => {
-          console.log(response);
-          this.employee = response;
-          this.getEmployees();
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-        }
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.employee = response;
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      }
     );
   }
 
   public onDeleteEmployee(employeeId: number): void {
     console.log(`Deleting employee by id: ${employeeId}`);
-      this.employeeService.deleteEmployee(employeeId).subscribe(
-        (response: void) => {
-          console.log(`Employee deleted`);
-          this.employee = response;
-          this.getEmployees();
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-        }
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(`Employee deleted`);
+        this.employee = response;
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      }
     );
   }
-  
+
   public onOpenModal(employee = null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
@@ -158,11 +132,10 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
 
-    if (container != null){
+    if (container != null) {
       container.appendChild(button);
     }
-    
+
     button.click();
   }
-
 }
